95c88d105dc87fea3a4e47b943a40a5b592bc3f5
Brayden England <bray.patriot@gmail.com>
2020-11-08 21:59:40 -0700

    Packages installed, basic files set up

---
+++ main.js
@@ -0,0 +1,19 @@
+const express = require('express')
+//const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const app = express()
+const port = process.env.PORT || 3000
+
+//middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+// mongoose.connect(process.env.MONGO_URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+
+app.listen(port, () => { console.log(`Server running on port ${port}`) })
+
+app.use('/', express.static('public'))
