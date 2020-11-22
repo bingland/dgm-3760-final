c1f1db5103c717aa35278ffc2d7d52dc2d44c9b6
Brayden England <bray.patriot@gmail.com>
2020-11-21 20:51:08 -0700

    List user reviews + SCSS, convert to stars + dates

--- public/main.js
+++ public/main.js
@@ -53,8 +53,9 @@ const focusDish = (id) => {
             <h1 class="focusName">${selectedDish.name}</h1>
             <div class="focusInfo">
                 <div class="focusRestaurant">${selectedDish.restaurant.name}</div>
-                <div class="focusRating">${calcRating(selectedDish.reviews.map(review => review.rating))} stars</div>
+                <div class="focusRating">${returnStars(calcRating(selectedDish.reviews.map(review => review.rating)))} stars</div>
                 <div class="focusReviewCount">${selectedDish.reviews.length} ${selectedDish.reviews.length == 1 ? 'Review' : 'Reviews'}</div>
+                <div class="focusPrice">${selectedDish.price}</div>
             </div>
         </header>
         <div class="focusPhotos">${selectedDish.pictures.map(pic => {
@@ -63,11 +64,16 @@ const focusDish = (id) => {
         <div class="focusReviews">
             ${selectedDish.reviews.map(review => {
                 return `
-                    <div class="reviewTitle">${review.title}</div>
+                <div class="reviewHeader">
+                    <div class="reviewPFP"><img src="./images/anonProfilePic.png" alt="user profile picture"></img></div>
                     <div class="reviewUser">${review.user.username}</div>
-                    <div class="reviewRating">${review.rating} stars</div>
-                    <div class="reviewDate">${review.date}</div>
+                    <div class="reviewRating">${returnStars(review.rating)}</div>
+                    <div class="reviewDate">${returnDate(review.date)}</div>
+                </div>
+                <div class="reviewContent">
+                    <div class="reviewTitle">${review.title}</div>
                     <div class="reviewBody">${review.body}</div>
+                </div>
                 `
             })}
         </div>
@@ -76,6 +82,32 @@ const focusDish = (id) => {
 
 const calcRating = (ratings) => {
     return ratings.reduce((a, b) => a + b) / ratings.length
+}
+
+const returnStars = (rating) => {
+    let counter = rating
+    let output = ''
+    
+    while (counter > 0) {
+        if (counter >= 0.75) {
+            output += '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z"/></svg>'
+        }
+        else if (counter < 0.75 && counter > 0.25) {
+            output += '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 5.173l2.335 4.817 5.305.732-3.861 3.71.942 5.27-4.721-2.524v-12.005zm0-4.586l-3.668 7.568-8.332 1.151 6.064 5.828-1.48 8.279 7.416-3.967 7.416 3.966-1.48-8.279 6.064-5.827-8.332-1.15-3.668-7.569z"/></svg>'
+        }
+        else {
+            output = 'error'
+        }
+        counter -= 1
+    }
+    return output
+}
+
+const returnDate = (string) => {
+    let millis = Date.parse(string)
+    let date = new Date(millis)
+    let output = date.toLocaleString('en-US', {month: 'long', day: 'numeric', year: 'numeric'})
+    return output
 }
 
 const getAllDishes = async () => {
