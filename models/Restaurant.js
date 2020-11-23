41de3e31c42b955626e248b19ed22074a7ee29b2
Brayden England <bray.patriot@gmail.com>
2020-11-22 20:09:27 -0700

    Updated model to have dishes be in an array

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose')
 
 const RestaurantSchema = new mongoose.Schema({
   name: String, 
-  dishes: {
+  dishes: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Dish'
-  }
+  }]
 }, { collection: 'restaurants' })
 
 module.exports = mongoose.model('Restaurant', RestaurantSchema)
